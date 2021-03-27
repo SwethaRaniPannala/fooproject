ee5109e0fa2a784ca787a6fd04384747d024a038
SwethaRani.Pannala@gmail.com <Tester@2021>
2021-03-27 16:11:04 +0100

    Added Missing Employee file

---
+++ src/main/java/Employee/Employee.java
@@ -0,0 +1,59 @@
+package Employee;
+
+public class Employee {
+    public int EmployeeId;
+    public String FirstName;
+    public String LastName;
+    public int Age;
+    public int CompanyId;
+    public double Salary;
+
+    public int getEmployeeId() {
+        return EmployeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        EmployeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public int getCompanyId() {
+        return CompanyId;
+    }
+
+    public void setCompanyId(int companyId) {
+        CompanyId = companyId;
+    }
+
+    public void setSalary(double salary) {
+        Salary = salary;
+    }
+
+    public double getSalary() {
+        return Salary;
+    }
+}
+
